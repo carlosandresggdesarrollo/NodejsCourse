3bed6b664c6245e3e90ae2b021a81383c1198b20
Andres <carlos.andres.g.g.desarrollo@gmail.com>
2021-08-18 22:50:09 -0500

    src/*

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+app.get('/', async (req, res, next) => {
+  res.status(200).send('Hello World! ')
+})
+app.listen(3000, () => console.log(' Server sis running on port 5000 '))
+
